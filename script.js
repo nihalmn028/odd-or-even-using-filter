e5e36941f8a903acc4b0e4836508219dde23bb90
nihalmn028 <148523248+nihalmn028@users.noreply.github.com>
2023-12-19 19:06:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let array=[1,2,3,4,5,6,7,8,9,10]
+const even=array.filter((n)=>n%2==0)
+const odd=array.filter((n)=>n%2==1)
+
+console.log("even numbers are: "+even)
+console.log("odd numbers are: "+odd)
+
